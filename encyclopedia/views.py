2453072e7a2847273f424984d22105f7f26616bb
Olha Romanova <larklarky@gmail.com>
2020-07-02 17:13:32 +0200

    add wiki entry page, 404 page and links from index page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 from . import util
 
@@ -6,5 +7,14 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    if util.get_entry(title) == None:
+        raise Http404('Page not found')
+    else:
+        return render(request, 'encyclopedia/entry.html', {
+            'title': title,
+            'entry': util.get_entry(title)
     })
 
