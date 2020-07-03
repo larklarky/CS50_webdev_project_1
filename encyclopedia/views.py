d0d5be47b3935d85a04d8dba4728dc3046d676bd
Olha Romanova <larklarky@gmail.com>
2020-07-03 17:25:50 +0200

    implement search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,20 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
 
 from . import util
 
 
 def index(request):
+    search_result = []
+    q = request.GET.get('q')
+    entries = util.list_entries()
+    if not q:
+        search_result = entries
+        return render(request, "encyclopedia/index.html", {
+            "entries": search_result
+        })
+    q = q.lower()
+    for entry in entries:
+        if q == entry.lower():
+            return HttpResponseRedirect(reverse('entry', args=[q]))
+        elif q in entry.lower():
+            search_result.append(entry)
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": search_result
     })
 
 def entry(request, title):
-    if util.get_entry(title) == None:
+    entry = util.get_entry(title)
+    if entry is None:
         raise Http404('Page not found')
     else:
         return render(request, 'encyclopedia/entry.html', {
             'title': title,
-            'entry': util.get_entry(title)
+            'entry': entry
     })
 
